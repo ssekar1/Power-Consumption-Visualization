a2da49a3cf47f6c8377b811430825a8c52f6e5c0
Chris <cgress1@umbc.edu>
2014-11-06 20:22:54 -0500

    working on routes

--- server.js
+++ server.js
@@ -98,11 +98,6 @@ var SampleApp = function() {
     self.createRoutes = function() {
         self.routes = { };
 
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
         self.routes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
