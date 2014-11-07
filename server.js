32f763028e0a9de5f9d530af8dcea0f558189fae
Chris <cgress1@umbc.edu>
2014-11-06 20:34:04 -0500

    Revert "still fiddling with routes"
    This reverts commit e0a5ae009c2cfa2fbcbc70d40b056f402a64f6b2.

--- server.js
+++ server.js
@@ -98,11 +98,6 @@ var SampleApp = function() {
     self.createRoutes = function() {
         self.routes = { };
 
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-
 	self.routes['/data'] = function(req, res) {
             res.setHeader('Content-Type', 'application/json');
             res.send("{ someVar: 3}");
