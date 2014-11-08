7a9601884e598e8a38ead966dfa6e52ea48f21ff
Chris <cgress1@umbc.edu>
2014-11-08 14:20:42 -0500

    fixed it?

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ var SampleApp = function() {
 		console.log(queryString);
 		selectedCircuits.forEach(function(item) {
 			console.log(item);
-			queryString += circuits[circuits[parseInt(item, 10)]] + ", ";
+			queryString += circuits[parseInt(item, 10)] + ", ";
 			console.log(queryString);
 		});
 		
