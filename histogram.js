23a2d324739ef7ba83b8b7152925c751ac809cef
Chris <cgress1@umbc.edu>
2014-11-23 22:03:29 -0500

    testing click

--- histogram.js
+++ histogram.js
@@ -72,7 +72,7 @@ function histogram(callback)
 			.attr("width", x(data[0].dx) - 1)
 			.attr("height", height)
 			.attr("style", "fill:white;")
-			.on("click", function (d){callback({domain: { start: histogramStartRange, end: histogramEndRange}, numBins: histogramNumberOfBins}));
+			.on("click", function (d){callback(););
 		
 		bar.append("rect")
 		    .attr("x", 1)
